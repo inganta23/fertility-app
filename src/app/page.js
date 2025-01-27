66b0afad24a27b0ec3a5d9213e674e863ddc55cd
Johanes Inganta Karo-Karo <johanes.karo@bithealth.co.id>
2025-01-27 11:35:57 +0700

    redirect home routes and change footer to client side

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
-import Card from "@/components/shared/Card";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <Card />
-    </div>
-  );
+  redirect("/courses");
 }
